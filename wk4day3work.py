ad27bbfb86e601ce96905a08bda5db196968324e
Pipperonni <blueas2019@gmail.com>
2023-02-15 22:37:39 -0600

    first commit

---
+++ wk4day3work.py
@@ -0,0 +1,173 @@
+num = [3,1,5,2,4,7,9,6,8]
+
+def sort_decorator(*args):
+    def inner(func):
+        re = sorted(func)
+        return re
+    return inner
+
+@sort_decorator
+def take_list(a_list):
+    return 
+
+class Node:
+
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+class LinkedList:
+
+    def __init__(self, head=None):
+        self.head = head
+    
+    def insert_before(self, value, target_value=None):
+        new_node = Node(value)
+        if isinstance(value, list):
+            for val in value: 
+                new_node = Node(val)
+                if not target_value:
+                    if self.head:
+                        new_node.next_node = self.head
+                    self.head = new_node
+                    
+                else:
+                    current_node = self.head
+                    previous_node = None
+                    
+                    while current_node:
+                        if current_node.value == target_value:
+                            previous_node.next_node = new_node
+                            new_node.next_node = current_node
+                            break
+                        
+                        previous_node = current_node
+                        current_node = current_node.next_node
+                    
+        elif not target_value:
+            if self.head:
+                new_node.next_node = self.head
+            self.head = new_node
+                
+        else:
+            current_node = self.head
+            previous_node = None
+            
+            while current_node:
+                if current_node.value == target_value:
+                    previous_node.next_node = new_node
+                    new_node.next_node = current_node
+                    break
+                
+                previous_node = current_node
+                current_node = current_node.next_node    
+        return new_node.value
+
+    def remove(self, value):
+        current_node = self.head
+        previous_node = None
+        
+        while current_node:
+            if current_node.value == value:
+                if not previous_node:
+                    self.head = current_node.next_node
+                else:    
+                    previous_node.next_node = current_node.next_node
+                break
+
+            previous_node = current_node
+            current_node = current_node.next_node
+                
+
+    def sum_num(self):
+        current_node = self.head
+        sum = 0
+        while current_node:
+            sum += current_node.value
+            current_node = current_node.next_node
+        return sum
+
+    def insert_after(self, value, target_value=None):
+        new_node = Node(value)
+        if isinstance(value, list):
+            for val in value: 
+                new_node = Node(val)
+                if not target_value:
+                    if self.head:
+                        current_node = self.head
+                        
+                        while current_node.next_node:
+                            current_node = current_node.next_node
+                        current_node.next_node = new_node
+                    
+                    else:
+                        self.head = new_node
+
+                else:
+                    current_node = self.head
+                    
+                    while current_node:
+                        if current_node.value == target_value:
+                            new_node.next_node = current_node.next_node
+                            current_node.next_node = new_node
+                            
+                            break
+                        
+                        current_node = current_node.next_node
+        
+        elif not target_value:
+            if self.head:
+                current_node = self.head
+                
+                while current_node.next_node:
+                    current_node = current_node.next_node
+                current_node.next_node = new_node
+            
+            else:
+                self.head = new_node
+
+        else:
+            current_node = self.head
+            
+            while current_node:
+                if current_node.value == target_value:
+                    new_node.next_node = current_node.next_node
+                    current_node.next_node = new_node
+                    
+                    break
+                
+                current_node = current_node.next_node
+
+    def contains(self, value):
+        
+        current_node = self.head
+
+        while current_node:
+            if current_node.value == value:
+                return True
+            current_node = current_node.next_node
+
+        return False
+
+    def print_nodes(self):
+        curreent_node = self.head
+        while curreent_node:
+            print(curreent_node.value, end=" -> ")
+
+            curreent_node = curreent_node.next_node
+        
+        print("None")
+    
+    @sort_decorator
+    def sort_list(self):
+        curreent_node = self.head
+        while curreent_node:
+            re_turn = curreent_node.value
+            curreent_node = curreent_node.next_node
+        
+        return re_turn
+
+my_list = LinkedList()
+my_list.insert_after(take_list(num))
+# my_list.insert_before(3,3)
+my_list.print_nodes()
